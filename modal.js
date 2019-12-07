2b65f0aee56c38bbaf0131f486f3bbeb63fb46d1
lkm1754 <57118585+lkm1754@users.noreply.github.com>
2019-12-06 21:45:44 -0500

    Adding modal image box for testing

--- modal.js
+++ modal.js
@@ -1,20 +1,25 @@
 function openModal(fileName) {
-//    var modal = document.getElementById('theModal');
-//    if (modal.style.display !== 'block'){
-//        pic = document.createElement('img')
-//        pic.src = fileName;
-//        pic.setAttribute('class','slide');
-//        pic.setAttribute('id','theSlide');
-//        pic.setAttribute('onclick','closeModal()');
-//        modal.appendChild(pic);
-//        modal.style.display = 'block';
-//    }
+    var mobile = window.innerWidth < 801;
+    var modal = document.getElementById('theModal');
+    var body = document.getElementsByTagName('body');
+    if (modal.style.display !== 'block' && !mobile){
+        body[0].style.margin = 0;
+        pic = document.createElement('img')
+        pic.src = fileName;
+        pic.setAttribute('class','slide');
+        pic.setAttribute('id','theSlide');
+        pic.setAttribute('onclick','closeModal()');
+        modal.appendChild(pic);
+        modal.style.display = 'block';
+    }
     
 }
 
 function closeModal() {
     var modal = document.getElementById('theModal');
     var pic = document.getElementById('theSlide');
+    var body = document.getElementsByTagName('body');
     modal.removeChild(pic);
     modal.style.display = 'none';
+    body[0].style.margin = '8px';
 }
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // var colors = ['#e6e5e5','#8f9779','#c9c0bb','#c8d3e2','#bda472','#cad4de','#decad4'];
-
+//
 // var place = 0;
-
+//
 // function changeColor() {
 //   document.body.style.background = colors[place];
 //   if (place < 6) {
@@ -10,11 +10,11 @@
 //     place = 0;
 //   }
 // }
-
+//
 // var fonts = ['Asap','Dosis','Montserrat','Nunito','Open Sans','PT Serif','Playfair Display'];
-
+//
 // var i = 0;
-
+//
 // function changeFont() {
 //   document.body.style.fontFamily = fonts[i];
 //   if (i < 6) {
